9df34ba61879acd2ddd3a04e2c49dd6d9735f3aa
Gustavo Romero <gromero@tutanota.com>
2019-12-17 09:02:45 -0300

    Fix simple serial TX
    This commit fixes raw serial to avoid conflicts w/ SystemVerilog
    syntax by removing 'bit' identifier since it's a keyword on SV. It
    also fixes counters based on simulation results.

--- raw_serial.v
+++ raw_serial.v
@@ -1,14 +1,14 @@
-module raw_serial(input clk, output txd);
+module raw_serial(input clk, output reg txd);
 
-reg [7:0] counter;
+reg [7:0] counter = 0;
 wire tx_clk;
 
 reg [1:0] state = 0;
 reg [7:0] byte = 65; // 'A'
-reg [2:0] bit = 0;
+reg [2:0] bitz = 0;
 
 always @ (posedge clk) begin
-  if (counter == 255) begin
+  if (counter == 103) begin
     counter = 0;
     tx_clk = 1;
   end else begin
@@ -21,25 +21,21 @@ always @ (posedge tx_clk) begin
   case (state)
     0:
        begin
-         txd = 1; 
-         state = 1;
-       end  
-
-    1:
-       if (bit < 8) begin
-         txd = byte[bit];
-         bit = bit + 1;
-       end else begin
-         bit = 0;
-         state = 2;
-       end
-
-    2: if (bit < 2) begin
          txd = 0;
-         bit = bit + 1;
+         state = 1;
+         bitz = 0;
+       end
+    1:
+       if (bitz == 7) begin
+         txd = byte[bitz];
+         state = 2;
        end else begin
-         bit = 0;
-         state = 0;
+         txd = byte[bitz];
+         bitz = bitz + 1;
+       end
+    2: begin
+         txd = 1;
+	 state = 0;
        end
   endcase
 end
