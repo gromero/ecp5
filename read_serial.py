ebe7ba7f328a6e320cb114c7fb8a7ea5faf22b73
Gustavo Romero <gromero@tutanota.com>
2020-01-15 18:59:04 -0300

    Add simple Python script to read from serial 1

---
+++ read_serial.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import serial
+
+# configure the serial connections
+ser = serial.Serial(
+    port='/dev/ttyUSB1',
+    baudrate=19200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS
+)
+
+
+while 1:
+    while ser.inWaiting() > 0:
+        byte = ser.read(1);
+        print("%s" %(byte))
