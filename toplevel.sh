feba43eaec05967facb40edb64c09f49ee4ab821
Gustavo Romero <gromero@tutanota.com>
2020-01-19 13:21:02 -0300

    UART: Improve script used to flash the bitstream

--- toplevel.sh
+++ toplevel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yosys -p "synth_ecp5 -json toplevel.json" toplevel.v
-nextpnr-ecp5 --json toplevel.json --textcfg toplevel_out.config --um5g-85k --package CABGA381 --lpf toplevel.lpf --freq 4 # XXX: freq = 4 just to make router pass wo/ error!
-ecppack --svf toplevel.svf toplevel_out.config toplevel.bit
+yosys -p "synth_ecp5 -json toplevel.json" toplevel.v &&
+nextpnr-ecp5 --json toplevel.json --textcfg toplevel_out.config --um5g-85k --package CABGA381 --lpf toplevel.lpf --freq 4 && # XXX: freq = 4 just to make router pass wo/ error!
+ecppack --svf toplevel.svf toplevel_out.config toplevel.bit &&
 sudo --preserve-env=PATH env openocd -f ./ecp5.cfg -c "transport select jtag; init; svf toplevel.svf; exit"
