d9d2df65bfa6d827dd30b278a330d3e9aa9abb37
Gustavo Romero <gromero@tutanota.com>
2020-06-01 21:26:27 -0300

    Add simulation to test RX part

--- uart_sim_tl.v
+++ uart_sim_tl.v
@@ -1,4 +1,5 @@
 `include "uart.v"
+// `timescale 1ns/1ns
 
 module uart_sim_tl;
 
@@ -6,22 +7,44 @@ reg master_clock = 0;
 reg wb_clock = 0;
 always #1 master_clock = !master_clock;
 // always #4 wb_clock = !wb_clock;
+// always #50 master_clock = !master_clock;
 
 reg rst;
 wire txd;
-output wire rxd;
+// output wire rxd;
 reg [1:0] data_addr;
 reg [7:0] data_in;
 wire [7:0] data_out;
 reg we;
 reg cs;
 wire ack_bit;
+reg rxd = 1;
 
   initial begin
     $dumpfile("uart_dumpfile.vcd");
     $dumpvars(0, uart_sim_tl, serial0, serial0.tx_fifo0);
+
+    // Send 0x47 'G' 0b0100_0111
+    #1000 rxd = 0;            // start bit
+    #(2*1248) rxd = 1;
+    #(2*1248) rxd = 0;
+    #(2*1248) rxd = 1;
+    #(2*1248) rxd = 0;
+    #(2*1248) rxd = 1;
+    #(2*1248) rxd = 0;
+    #(2*1248) rxd = 1;
+    #(2*1248) rxd = 0;
+    #(2*1248) rxd = 1; // stop bit
+
+    #3000 data_addr = 1; // RX_ADDR
+    #100 we = 1;         // READ
+    #100 cs = 1;         // CS
+    #100 wb_clock = 1;   // 1 cycle to wishbone bus
+    #100 wb_clock = 0;
+
+/*
     #10 data_addr = 0;
-    #10 data_in = 65;
+    #10 data_in = 68; // 'B'
     #0 rst = 1;
     #2 rst = 0; // off reset
     #2 we = 0;
@@ -29,7 +52,20 @@ wire ack_bit;
     #2 wb_clock = 1;
     #2 wb_clock = 0;
     #2 cs = 0; // de select IC
-    #4000 $stop;
+
+    // Send 0x47 'G' 0b0100_0111
+    #22530 rxd = 0; // start bit
+    #25060 rxd = 1;
+    #27590 rxd = 1;
+    #30120 rxd = 1;
+    #32650 rxd = 0;
+    #35180 rxd = 0;
+    #37710 rxd = 0;
+    #40240 rxd = 1;
+    #42770 rxd = 0;
+    #45300 rxd = 1; // stop bit
+*/
+    #50000 $stop;
   end
 
 uart serial0(
