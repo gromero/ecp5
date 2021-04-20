3f89b6d5e8fca9154be03cdb57b51b8289ddb8d3
Supertropics <supertropics@tuta.io>
2021-04-20 09:04:18 -0300

    Cleanup left over
    This is a force

--- main.v
+++ main.v
@@ -2,13 +2,6 @@
 
 module main (input clk, input reset, output full, output empty, input[7:0] data_in, output[7:0] data_out, input push, input pop, output [7:0] data_out2led, input [3:0] unused);
 
-/*
-module fifo(clk, reset, full, empty, data_in, data_out, push, pop);
-*/
-
-// RPi3 # of used pins = 1 + 2 + 16 + 2 = 21 -> 26 - 21 = 4 unused
-// ECP5 # of used pins = 1 + 8 = 9
-
 fifo fifo0(
   .clk(clk),
   .reset(reset),
